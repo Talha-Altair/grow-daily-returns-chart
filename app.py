9a356784595a295a8540c7b499a043dbb673e66b
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-06-09 17:11:15 +0530

    Updates for 2021-06-09

---
+++ app.py
@@ -0,0 +1,34 @@
+'''
+Source:
+
+Author: Talha
+
+
+'''
+from flask import Flask,render_template, jsonify, request
+import random
+import json
+import pandas as pd 
+
+
+app  = Flask(__name__)
+PORT = 3000
+
+@app.route("/", methods=["GET","POST"])
+def startpy():
+    
+    pnl_list = get_data()
+
+    return render_template("date.html",pnl_list=pnl_list) 
+
+
+def get_data():
+    
+    df = pd.read_csv('pnl.csv')
+
+    pnl_list    = df['pnl'].tolist()
+
+    return pnl_list
+
+if __name__ == "__main__":
+    app.run(debug = True,host="0.0.0.0",port = PORT)
